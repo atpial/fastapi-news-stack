a4d05ff5d3e56a0355d3cc7cf5888adc19738578
atpial <atp82232@gmail.com>
2025-04-20 01:23:20 +0600

    feat: added dockerfile & updated config

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
@@ -11,13 +12,19 @@ class Settings(BaseSettings):
     DATABASE_USER: str = Field(..., env="DATABASE_USER")
     DATABASE_PASSWORD: str = Field("", env="DATABASE_PASSWORD")
     DATABASE_HOST: str = Field("127.0.0.1", env="DATABASE_HOST")
+    DATABASE_HOST_DOCKER: str = Field(
+        "host.docker.internal", env="DATABASE_HOST_DOCKER"
+    )
     DATABASE_PORT: int = Field(3306, env="DATABASE_PORT")
 
     @property
     def DATABASE_URL(self):
+        host = self.DATABASE_HOST
+        if os.path.exists("/.dockerenv"):
+            host = self.DATABASE_HOST_DOCKER
         return (
             f"mysql+pymysql://{self.DATABASE_USER}:{self.DATABASE_PASSWORD}"
-            f"@{self.DATABASE_HOST}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
+            f"@{host}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
         )
 
     class Config:
